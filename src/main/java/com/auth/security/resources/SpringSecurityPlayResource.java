a274e68f64217a998eb8c22e6b6df9bb1048270a
Matthew Nicholson <thematthewnicholson@gmail.com>
2024-11-09 10:01:54 -0800

    add csrf

---
+++ src/main/java/com/auth/security/resources/SpringSecurityPlayResource.java
@@ -0,0 +1,16 @@
+package com.auth.security.resources;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class SpringSecurityPlayResource {
+    
+    @GetMapping("/csrf-token")
+    public CsrfToken retrieveCsfrToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
