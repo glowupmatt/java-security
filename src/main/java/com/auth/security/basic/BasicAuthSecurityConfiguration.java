8a4cd08ec2136f4ae4a33835c811405c96ac0b92
Matthew Nicholson <thematthewnicholson@gmail.com>
2024-11-09 13:15:12 -0800

    Add JDBC support and configure CORS in security setup

--- src/main/java/com/auth/security/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/auth/security/basic/BasicAuthSecurityConfiguration.java
@@ -1,11 +1,17 @@
 package com.auth.security.basic;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -17,5 +23,36 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic(withDefaults());
         http.csrf().disable();
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
+    enum Role {
+        USER,ADMIN
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+        var user = User.withUsername("user")
+        .password("{noop}dummy")
+        .roles(Role.USER.toString())
+        .build();
+        
+        var admin = User.withUsername("admin")
+        .password("{noop}dummy")
+        .roles(Role.ADMIN.toString())
+        .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
